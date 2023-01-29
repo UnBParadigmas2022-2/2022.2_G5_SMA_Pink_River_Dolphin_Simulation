5c62d21c1525859186e398c73a87db4b6875524d
Victor Rayan <victor.rayan378@gmail.com>
2023-01-28 23:23:01 -0300

    GUI mesa description

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from src.PinkDolphinAgent import PinkDolphinAgent
 from src.WaterAgent import WaterAgent
 from src.Model import Model
 
+descricao = "Seguindo a temática de animais em risco de extinção aplicada nas duas últimas entregas, \
+decidimos desenvolver um sistema que simule as rotas migratórias do Boto Cor de Rosa, \
+tendo como base determinadas complicações por nós estabelecidas, \
+como a qualidade da água, a disponibilidade de comida e a presença de pescadores."
 
 def portrayal_method(agent):
     portrayal = {"Filled": "true", "Layer": 0, "w": 1, "h": 1}
@@ -22,5 +26,6 @@ grid = CanvasGrid(portrayal_method, 15, 15, 550, 550)
 server = ModularServer(
     Model, [grid], "Pink Dolphin", {"num_agents": 25, "width": 15, "height": 15}
 )
+server.description = descricao
 server.port = 8001
 server.launch()
